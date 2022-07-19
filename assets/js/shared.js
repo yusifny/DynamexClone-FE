88fb362c53a9259d9b7bd1fb7925b51f12ba1284
Yusif Nazarbayov <tu4rwlg2x@code.edu.az>
2022-07-19 22:57:59 +0400

    Language selection script migrated from about.js to shared.js

---
+++ assets/js/shared.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $("#lang-all").click(function(){
+        $("#lang-drop-down").toggleClass("open");
+        $("#chevron").toggleClass("chevron-toggle");
+    });
+    $("#aze").click(function(){
+        $("#aze").toggleClass("selected");
+        $("#eng").removeClass("selected");
+        $("#rus").removeClass("selected");
+        $("#lang").text("AZ");
+    });
+    $("#eng").click(function(){
+        $("#eng").toggleClass("selected");
+        $("#aze").removeClass("selected");
+        $("#rus").removeClass("selected");
+        $("#lang").text("EN");
+    });
+    $("#rus").click(function(){
+        $("#rus").toggleClass("selected");
+        $("#aze").removeClass("selected");
+        $("#eng").removeClass("selected");
+        $("#lang").text("RU");
+    });
+});
