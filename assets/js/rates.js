d7bf297a6a57cd0dc339a90d90f6bb1425acfdbf
Yusif Nazarbayov <tu4rwlg2x@code.edu.az>
2022-07-23 23:54:26 +0400

    Refactor: efficiency increased

--- assets/js/rates.js
+++ assets/js/rates.js
@@ -2,26 +2,24 @@ $(document).ready(function(){
     $('.türkiye').click(function(){
         $(".usa").removeClass("active");
         $(this).addClass("active");
-        
+        $(".from-usa").hide();
+        $(".from-türkiye").fadeIn("slow");    
     });
     $('.usa').click(function(){
         $(".türkiye").removeClass("active");
         $(this).addClass("active");
+        $(".from-türkiye").hide();
+        $(".from-usa").fadeIn("slow");
     });
     $('.branch').click(function(){
         $(".home").removeClass("focus");
         $(this).addClass("focus");
-        
+        $(".to-address").hide();
+        $(".to-branch").fadeIn("slow");  
     });
     $('.home').click(function(){
         $(".branch").removeClass("focus");
         $(this).addClass("focus");
-    });
-    $('.branch').click(function(){
-        $(".to-address").hide();
-        $(".to-branch").fadeIn("slow");
-    });
-    $('.home').click(function(){
         $(".to-branch").hide();
         $(".to-address").fadeIn("slow");
     });
